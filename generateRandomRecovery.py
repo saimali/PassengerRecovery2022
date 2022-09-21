bcc6266df757843aed917f5a9a4e13299ed4d9f7
Sai Mali Ananthanarayanan <sa3517@columbia.edu>
2022-09-21 14:36:33 -0400

    randomrecovery generator doesn't delay all flights
    60% of sampled flights are delayed,
    40% are the same
    
    We are sampling 50% of all flights

--- generateRandomRecovery.py
+++ generateRandomRecovery.py
@@ -1,12 +1,28 @@
+#%%
 import pandas as pd
 import numpy as np
+import random
 from functionsPax_Sept2022 import *
 from datetime import date, timedelta
+import fileinput
 
+#%%
+# disruption date
 defaultDate = '07/01/06'
 
+#%%
 def randomDelay(dummy):
-    return timedelta(hours=np.random.randint(0,4), minutes=np.random.randint(0,60))
+    
+    # positive delay minutes
+    posDelaynumber = timedelta(hours=np.random.randint(0,7), minutes=np.random.randint(0,60))
+    # no delay
+    noDelay = timedelta(hours=0, minutes=0)
+    
+    # choose a delay time among two options
+    twoDelayOptions = [posDelaynumber,noDelay]
+    
+    # return no delay wp 0.4, some pos delay wp 0.6
+    return random.choices(twoDelayOptions,[0.6,0.4])[0]
 
 def addTime(inputTime, delay):
 
@@ -26,11 +42,19 @@ flightsDataFr = pd.read_csv('flights.csv',
                             names=['Num','Orig','Dest','DepTime','ArrTime','PrevFlightBySameAircraft'],
                             delim_whitespace=True)
 
+bad_rows = flightsDataFr['Num'].str.contains('#')
+
+flightsDataFr = flightsDataFr[~bad_rows]
+
+
 flightsDataFr['Delay'] = flightsDataFr['Orig'].apply(randomDelay)
 
-flightsRecovered = flightsDataFr.sample(frac=0.8)
+flightsRecovered = flightsDataFr.sample(frac=0.5)
 flightsRecovered['DepTime'] = flightsRecovered.apply(lambda x: addTime(x['DepTime'], x['Delay']), axis=1)
 flightsRecovered['ArrTime'] = flightsRecovered.apply(lambda x: addTime(x['ArrTime'], x['Delay']), axis=1)
 
 flightsRecovered.drop('Delay', axis=1, inplace=True)
 flightsRecovered.to_csv('random_recovered_flights.csv', index=False, header=False, sep=" ")
+
+with open("random_recovered_flights.csv","a") as myfile:
+    myfile.write("#")
